c51d236e46eea6c53db9ad06dd84d91033030c9e
larkloss <dingzhao1995@gmail.com>
2021-10-25 14:29:31 +0800

    update: money tab的框架和样式搭建（也可以复用到其他tab下

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,11 +1,25 @@
 import Layout from '../components/Layout';
 import React from 'react';
+import {TagsSection} from "./Money/TagsSection";
+import {NotesSection} from "./Money/NotesSection";
+import {CategorySection} from "./Money/CategorySection";
+import {NumberPadSection} from "./Money/NumberPadSection";
+import styled from "styled-components";
+
+
+const MyLayout = styled(Layout)`
+  display:flex;
+  flex-direction: column;
+`
 
 function Money() {
     return (
-        <Layout>
-            <h2>记账页面</h2>
-        </Layout>
+        <MyLayout>
+            <TagsSection></TagsSection>
+            <NotesSection></NotesSection>
+            <CategorySection></CategorySection>
+            <NumberPadSection></NumberPadSection>
+        </MyLayout>
     );
 }
 
